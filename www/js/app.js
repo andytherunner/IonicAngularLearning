68cd853c189b225beb29532ea2914d06ef22f839
Andy Roberts <andy.roberts@patlive.com>
2015-09-29 22:53:11 -0400

    Working on the calendar

--- www/js/app.js
+++ www/js/app.js
@@ -54,8 +54,45 @@ angular.module('starter', ['ionic'])
             }
         })
 
+        .state('tabs.calendar', {
+            url: '/calendar',
+            views: {
+                'calendar-tab' : {
+                    templateUrl: 'templates/calendar.html',
+                    controller: 'CalendarController'
+                }
+            }
+        })
+
     $urlRouterProvider.otherwise('/tab/home');
 })
+
+
+    .controller('CalendarController', ['$scope', '$http','$state',
+        function($scope, $http, $state){
+            $http.get('js/data.json').success(function(data) {
+                $scope.calendar = data.calendar;
+
+                $scope.data = {showDelete: false,
+                    showReorder: false}
+
+                $scope.onItemDelete = function(dayIndex, item){
+                    $scope.calendar[dayIndex].schedule.splice($scope.calendar[dayIndex].schedule.indexOf(item),1);
+                };
+
+                $scope.toggleStar = function(item){
+                    item.star = !item.star;
+                };
+                $scope.doRefresh = function(){
+                    $http.get('js/data.json').success(function(
+                        data) {
+                        $scope.calendar = data.calendar;
+                        $scope.$broadcast('scroll.refreshComplete'
+                        );
+                    });
+                };
+            })
+        }])
 
 .controller('ListController', ['$scope', '$http','$state',
     function($scope, $http, $state){
